040a956a6057564684d28505906d424e9a43ec31
Akbar <isroilovakbar49@gmail.com>
2023-01-07 19:32:16 +0500

    masalalar

---
+++ main.js
@@ -0,0 +1,438 @@
+// const form = document.querySelector(".form")
+
+// const input = document.querySelector(".input")
+// const text = document.querySelector(".text")
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     if (input.value == 'asus')
+//         text.innerHTML = '500$'
+
+//     else if (input.value == 'lenovo')
+//         text.innerHTML = '550$'
+// })
+
+
+
+
+
+// 1-masala
+
+// function findnumber(a) {
+//     if (a > 0) {
+//         console.log("1-bu musbat");
+//     } else {
+//         console.log("2-musbat emas");
+//     }
+// }
+// findnumber(0);
+
+
+
+
+// 2-masala
+
+// function findtoq(a) {
+//     if (a % 2 == 0) {
+//         console.log("juft");
+//     }
+//    else {
+//         console.log("toq");
+//     }
+
+// }
+// findtoq(9)
+
+
+// 3-masala 
+
+// function findtoq(a) {
+//     if (a % 2 == 0) {
+//         console.log("juft");
+//     }
+//    else {
+//         console.log("toq");
+//     }
+
+// }
+// findtoq(9)
+
+
+// 4-masala 
+
+// function twoonumber(a,b) {
+//     if (a>0 && b>0) {
+//         console.log("ikkalasi musbat");
+//     }
+//     else{
+//         console.log("ikkalasi musbat emas ");
+//     }
+// }
+// twoonumber(4,-5)
+
+
+// 5-masala
+
+
+// function check(a,b,c) {
+//     if (a>b && b>c) {
+//         console.log("ros");
+//     }
+//     else{
+//         console.log("yolgon");
+//     }
+// }
+// check(5,4,3)
+
+
+
+// 6-masala
+
+// function check(a,b,c) {
+//     if (a>b && b>c) {
+//         console.log("ros");
+//     }
+//     else{
+//         console.log("yolgon");
+//     }
+// }
+// check(5,4,3)
+
+
+// 7-masala
+
+// function check(a, b, c) {
+//     if (a % 2 == 0) {
+//         console.log("a juft");
+//     }
+//     if (a % 2 == 1) {
+//         console.log("a toq");
+//     }
+//     if (b % 2 == 0) {
+//         console.log("b juft");
+//     }
+//     if (b % 2 == 1) {
+//         console.log("b toq");
+//     }
+//     if (a % 2 == 0) {
+//         console.log("c juft");
+//     }
+//     if (a % 2 == 1) {
+//         console.log("c toq");
+//     }
+// }
+// check(2,3,4)
+
+
+
+//   8-masala
+
+// function check(a, b, c, d) {
+//     if (a > 0 && b > 0 && c > 0 && d > 0) {
+//      console.log("barchasi musbat");
+//     }
+//     else {
+//         console.log("sonlardan biri manfiy yoki barchasi");
+//     }
+// }
+
+// check(5,6,7,9)
+
+
+
+// 9-masala
+
+// function check(a, b, c) {
+//     if (a > 0 && b > 0) {
+//         console.log("ikkitasi musbat");
+//     } else if (a > 0 && c > 0) {
+//         console.log("ikkitasi musbat");
+//     } else if (b > 0 && c > 0) {
+//         console.log("ikkitasi musbat");
+//     } else {
+//         console.log("yolgon");
+//     }
+
+// }
+// check(4, -5, 6)
+
+
+
+// 10-masala
+
+
+// function findweekday(num) {
+//     if (num==1) {
+//         console.log("duwanba");
+//     }
+//     if (num==2) {
+//         console.log("sewanba");
+//     }
+//     if (num==3) {
+//         console.log("corwanba");
+//     }
+//     if (num==4) {
+//         console.log("paywanba");
+//     }
+//     if (num==5) {
+//         console.log("juma");
+//     }
+//     if (num==6) {
+//         console.log("wanba");
+//     }
+//     if (num==7) {
+//         console.log("yakwanba");
+//     }
+// }
+// findweekday(6)
+
+
+// 11-masala
+
+
+// function phonenumber(num) {
+//     if (num == 91 || num == 90) {
+//         console.log("beeline");
+//     } else if (num == 93 || num == 94) {
+//         console.log("ucell");
+//     } else if (num == 95 || num == 99 ||num==77) {
+//         console.log("uzmobile");
+//     } else if (num == 97 || num == 88) {
+//         console.log("mobiuz");
+//     }else if (num == 98) {
+//         console.log("Perfectum");
+//     }
+// }
+// phonenumber(77)
+
+
+
+// 12-masala
+// Butun son berilgan agar son musbat bo'lsa unda sonni bittaga oshiring aks xolda 1taga kamaytiring
+
+// function summa(a) {
+//     if (a > 0) {
+//        return a+1
+
+//     }
+//     else
+//     return a-1
+
+// }
+// console.log(summa(-10));
+
+
+// 13-masala
+
+// Butun son berilgan agar son musbat bo'lsa sonni 3 ga oshiring manfiy bo'lsa unda 2ga kamaytiring
+
+// function summa(a) {
+//     if (a>0) {
+//         return a+3
+//     }
+//     else
+//     return a-2
+// }
+// console.log(summa(-4));
+
+
+
+// 14-masala
+
+// Ikkita butun son berilgan kattasin aniqlovchi dastur tuzing
+
+// function check(a, b) {
+//     if (a > b) {
+//         console.log("a katta b dan");
+//     } else{
+//         console.log("a kichik b dan");
+//     }
+
+// }
+
+// check(6,8)
+
+
+// 15-masala
+
+
+
+
+
+// 16-masala
+
+// function check(a, b, c) {
+//     if (a > b && a > c) {
+//         console.log("a eng katta");
+//     }
+//     if (b > a && b > c) {
+//         console.log("b eng katta");
+//     }
+//     if (c > a && c > b) {
+//         console.log("c eng katta");
+//     }
+// }
+// check(2,3,4)
+
+
+// 17-masala
+
+
+// const form = document.querySelector(".form")
+
+// const input = document.querySelector(".input")
+// const img = document.querySelector(".img")
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     console.log(img);
+//     if (input.value == 'erkak')
+//         img.src = 'https://fuzeservers.ru/wp-content/uploads/7/4/6/74667c5e05004bdf83f789f890554694.jpeg'
+
+//     else
+//         img.src = 'https://i.pinimg.com/originals/e3/ae/55/e3ae55548c65e4cad90d60e25253e7bc.jpg'
+// })
+
+
+
+// 18-masala
+
+// function baxo(a) {
+//     if (a==3) {
+//         console.log("500.000");
+//     }
+//     else if (a==4) {
+//         console.log("600.000");
+//     }
+//     else if (a==5) {
+//         console.log("700.000");
+//     }
+//     else{
+//         console.log("siz iwlang");
+//     }
+
+// }
+// baxo(2)
+
+
+// 19-masala
+// Butun son berilgan shu songa qarab toq musbat, toq manfiy, juft musbat, juft manfiy deb chiqaruvchi dastur tuzing
+
+// function check(a) {
+//     if (a % 2 == 0 && a > 0) {
+//         console.log("juft musbat");
+//     } else if (a % 2 == 0 && a < 0) {
+//         console.log("juft manfiy");
+//     } else if (a % 2 == 1 && a > 0) {
+//         console.log("toq musbat");
+//     } else if (a % 2 == 1 && a < 0) {
+//         console.log("toq manfiy");
+//     }
+// }
+// check(5)
+
+
+
+// 20-masala
+
+
+// Yilga qarab yoshin aniqlovchi dastur tuzing
+
+
+
+// function age(a) {
+//     return 2023-a
+// }
+// console.log(age(2004)+" yoshdasiz");
+
+
+
+// 21-masala
+
+
+// 3 ta butun son berilgan jumlani rostlikkar tekshiring. A <= B <= C
+
+// function check(a, b, c) {
+//     if (a >= b && a >= c && b >= c) {
+//         console.log("rost");
+//     } else {
+//         console.log("yolgon");
+//     }
+// }
+// check(3,2,1)
+
+
+
+// 22-masala
+
+// ikki butun son berilgan ikki sondan faqat 1tasi toq son hisoblanadi
+
+
+// function num(a, b) {
+//     if (a % 2 == 1 && b % 2 == 0) {
+//         console.log("rost");
+//     }
+//     else if (b % 2 == 1 && a % 2 == 0) {
+//         console.log("rost");
+//     }
+//     else{
+//         console.log("yolgon");
+//     }
+// }
+// num(3,4)
+
+
+
+// 23-masala
+
+// a,b,c sonlar berilgan har biri musbat son
+
+// function check(a, b, c) {
+//     if (a > 0 && b < 0 && c < 0) {
+//         console.log("biri musbat");
+//     } else if (b > 0 && a < 0 && c < 0) {
+//         console.log("biri musbat");
+//     } else if (c > 0 && b < 0 && a < 0) {
+//         console.log("biri musbat");
+//     } else {
+//         console.log("yolgon");
+//     }
+// }
+// check(-5,6,-8)
+
+
+// 24-masala
+
+//     if (a > 0 && b < 0 && c < 0) {
+//         console.log("biri musbat");
+//     } else if (b > 0 && a < 0 && c < 0) {
+//         console.log("biri musbat");
+//     } else if (c > 0 && b < 0 && a < 0) {
+//         console.log("biri musbat");
+//     } else {
+//         console.log("yolgon");
+//     }
+// }
+// check(-5,6,-8)
+
+
+
+// 25-masala
+
+
+
+// 3 xonali son berilgan har bir raqamlari har xil
+
+// function num(son) {
+//     a = parseInt(son / 100)
+//     b = parseInt(son % 100 / 10)
+//     c = parseInt(son % 10)
+//     console.log(a, b, c);
+//     if (a != b && b != c && a != c)
+//         return true
+//     else
+//         return false
+// }
+// console.log(num(323));
+
+
